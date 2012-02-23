7d91c5df8a0ce4e8121e98e96bd30e4d41eb09a7
Twisted <twistedumbrella@gmail.com>
2012-02-23 13:27:46 -0500

    Add qualcomm vendor build toggle

--- Android.mk
+++ Android.mk
@@ -1,6 +1,10 @@
 OMX_CORE_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
+ifeq ($(BUILD_QCOM_VENDOR),true)
+
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include $(OMX_CORE_PATH)/omxcore/Android.mk
+endif
+
 endif
